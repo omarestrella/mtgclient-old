832c48197b733ee28209b840d5e9c3ba0aaa3eb4
Omar Estrella <omarestrella@bitcreative.net>
2013-12-11 21:13:51 -0500

    Added search

--- app/javascripts/application/ApplicationController.js
+++ app/javascripts/application/ApplicationController.js
@@ -1,3 +1,11 @@
 MTG.ApplicationController = Ember.Controller.extend({
+    search: null,
 
+    searchChanged: function () {
+        if(this.get('search')) {
+            this.set('content', this.store.find('card', { search: this.get('search') }));
+        } else {
+            this.set('content', this.store.find('card'));
+        }
+    }.observes('search')
 });
